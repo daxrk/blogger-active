2db0291e6d9377e7f4959d9ace7556970604fc7d
Mostafa Mohamed <77597243+daxrk@users.noreply.github.com>
2023-08-16 18:53:01 -0400

    Update and rename active1.js to active.js

--- active.js
+++ active.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     // Check if the current URL is not in the allowedUrls array
     if (!allowedUrls.includes(currentUrl)) {
         // Create an image element
-        var img = $("<img src='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjxxHJSKgGIrce0zsp_8gFya3xL9JXdOkCCbwoEE1mEg-B0judYkpGvzeZaN5RDBX2q-WTZIeMlXnaKi_KljlbMkP54JOWog5rGRKxA5DrX-m0q9pnQk8epohwD_V-wvwP9waSCAsP6aB2rV2l1Wezwi1htHqVMnUOWZy6I-D2OZ9l4lv7mMjywwHji838/s320/aglytch.jpg' alt='aglytch'></img>");
+        var img = $("<img src='https://i.postimg.cc/9zyw8JZB/photo-2023-08-16-18-26-14.jpg' alt='aglytch'></img>");
 
         // Replace the content of the body with the image
         $("body").empty().append(img);
