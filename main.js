0272af63bd9bb9bf4230c88971f48e443d29e45d
Mostafa Mohamed <77597243+daxrk@users.noreply.github.com>
2023-09-07 08:16:35 -0700

    Update and rename main3.js to main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var allowedUrls = ["localhost/webx/wordpress/", "www.anotherurl.com"];
+    var allowedUrls = ["http://localhost/webx/wordpress/", "www.anotherurl.com"];
     var currentUrl = window.location.hostname;
 
     // Check if the current URL is not in the allowedUrls array
