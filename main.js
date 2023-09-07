a8f5acc90016bf5747eeee89f02aab0567c88b83
Mostafa Mohamed <77597243+daxrk@users.noreply.github.com>
2023-09-07 08:14:48 -0700

    Update main1.js

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-$(document).ready(function () {
-    var allowedUrls = ["www.myurl.com", "www.anotherurl.com"]; // Add allowed URLs to this array
+document.addEventListener("DOMContentLoaded", function () {
+    var allowedUrls = ["www.myurl.com", "www.anotherurl.com"];
     var currentUrl = window.location.hostname;
-    
+
     // Check if the current URL is not in the allowedUrls array
     if (!allowedUrls.includes(currentUrl)) {
         // Create an image element
-        var img = $("<img src='aglytch.jpg' alt='aglytch'></img>");
-        
+        var img = document.createElement("img");
+        img.src = "aglytch.jpg";
+        img.alt = "aglytch";
+
         // Replace the content of the body with the image
-        $("body").empty().append(img);
+        var body = document.body;
+        body.innerHTML = ""; // Clear the body's content
+        body.appendChild(img);
     }
 });
-
